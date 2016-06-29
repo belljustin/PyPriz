8b388d679c50d781ce304d42646302f077998724
Justin Bell <justin.bell@mail.mcgill.ca>
2016-06-28 22:45:32 -0400

    New: single bots can now compete

---
+++ test_bots/hogbot.py
@@ -0,0 +1,25 @@
+'''
+    Hogbot
+    uses too much memory
+'''
+
+class Bot:
+    name = "Blabby Bot"
+
+    # store your bot's state here
+    def __init__(self):
+        pass
+
+    # define your bot's playing strategy here 
+    # True = betray, False = keep quiet
+    def play(self):
+        a = ['a']*100*10**6
+        return False
+
+    # use data from the previous round to update your strategy
+    def get_update(self, update):
+        return None
+
+    def __repr__(self):
+        return name
+
