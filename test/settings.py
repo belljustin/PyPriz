67d91b95d39b2a911abb1e0287ebfcf1226a5809
Justin Bell <justin.bell@mail.mcgill.ca>
2016-07-16 15:48:20 -0400

    yml file for Travis

--- test/settings.py
+++ test/settings.py
@@ -1,6 +1,7 @@
 from tempfile import mkdtemp
+import os
 
-SECRET_KEY = b'\x01x&\x17\xd3\xb3\xedD\xe3e(\t\x97m4P\x87\x049\xd3*\xec\xdb\x88'
+SECRET_KEY = b'CKRTQI'
 TESTING = True
 SERVER_NAME = 'pypriz.local:5000'
 
